d76f980a96bd8e5cc9f6f33d170c363b933ff6d4
MinhHieu212 <hieu.tranminhk21@hcmut.edu.vn>
2023-08-16 22:20:50 +0700

    Update reponsive done!

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 // Script for navigation bar
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+if(bar) {
+    bar.addEventListener('click', () => {
+        if(nav) nav.classList.add('active');
+    })
+}
+
+if(close) {
+    close.addEventListener('click', () => {
+        if(nav) nav.classList.remove('active');
+    })
+}
