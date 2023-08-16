4644c86e41fce2580e4a1adec58187cb82c7ba58
MinhHieu212 <hieu.tranminhk21@hcmut.edu.vn>
2023-08-17 01:02:12 +0700

    finish shop.html and sproduct.html

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // Script for navigation bar
+
 const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
 const close = document.getElementById('close');
@@ -14,3 +15,24 @@ if(close) {
         if(nav) nav.classList.remove('active');
     })
 }
+
+// sproduct
+
+mainImg = document.getElementById('MainImg');
+smallImg = document.querySelectorAll('.SmallImg');
+
+smallImg[0].onclick = () => {
+    mainImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = () => {
+    mainImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = () => {
+    mainImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = () => {
+    mainImg.src = smallImg[3].src;
+}
+
+//add event for href to sproduct
+
